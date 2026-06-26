a955201e2a0def36597224ddfc5466471b04e229
Ragul000 <vraguldharun@gmail.com>
2026-06-26 17:29:44 +0530

    fix

--- backend/src/controllers/whatsappController.js
+++ backend/src/controllers/whatsappController.js
@@ -113,6 +113,7 @@ const webhookPost = async (req, res) => {
         
         return res.status(200).send('EVENT_RECEIVED');
       }
+    }
 
     console.log('Webhook received:', JSON.stringify(req.body));
 
